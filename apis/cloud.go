8f66aac0431992e5357bf53386dcc4f8a9471f57
kongzZ <kongzz45@foxmail.com>
2022-04-21 18:13:54 +0800

    base done

--- apis/cloud.go
+++ apis/cloud.go
@@ -1,11 +1,71 @@
 package apis
 
-import "github.com/gin-gonic/gin"
+import (
+	"dongzhai/models"
+	"dongzhai/service"
+	"fmt"
+	"net/http"
+	"strconv"
 
-func CreateCloud(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+)
 
-func GetClouds(c *gin.Context) {}
+func CreateCloud(c *gin.Context) {
+	var cloud models.Cloud
+	if err := c.Bind(&cloud); err != nil {
+		Response(http.StatusBadRequest, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	if err := service.CreateCloud(cloud); err != nil {
+		Response(http.StatusInternalServerError, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	Response(http.StatusOK, "add cloud success", nil, c)
+}
 
-func UpdateCloud(c *gin.Context) {}
+func GetClouds(c *gin.Context) {
+	query := &models.Pagination{}
+	if err := c.ShouldBindQuery(query); err != nil {
+		Response(http.StatusBadRequest, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	clouds, total, err := service.GetClouds(query)
+	if err != nil {
+		Response(http.StatusInternalServerError, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	data := gin.H{
+		"page":  query.Page,
+		"size":  query.Size,
+		"total": total,
+		"data":  clouds,
+	}
+	Response(http.StatusOK, "get clouds success", data, c)
+}
 
-func DeleteCloudById(c *gin.Context) {}
+func UpdateCloud(c *gin.Context) {
+	var cloud models.Cloud
+	if err := c.Bind(&cloud); err != nil {
+		Response(http.StatusBadRequest, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	if err := service.UpdateCloud(cloud); err != nil {
+		Response(http.StatusInternalServerError, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	Response(http.StatusOK, "update cloud success", cloud, c)
+}
+
+func DeleteCloudById(c *gin.Context) {
+	id := c.Param("id")
+	cloud_id, err := strconv.Atoi(id)
+	if err != nil {
+		Response(http.StatusBadRequest, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	if err = service.DeleteCloudById(cloud_id); err != nil {
+		Response(http.StatusInternalServerError, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	Response(http.StatusOK, "delete cloud success", nil, c)
+}
--- apis/product.go
+++ apis/product.go
@@ -1,11 +1,71 @@
 package apis
 
-import "github.com/gin-gonic/gin"
+import (
+	"dongzhai/models"
+	"dongzhai/service"
+	"fmt"
+	"net/http"
+	"strconv"
 
-func CreateProduct(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+)
 
-func GetProducts(c *gin.Context) {}
+func CreateProduct(c *gin.Context) {
+	var product models.Product
+	if err := c.Bind(&product); err != nil {
+		Response(http.StatusBadRequest, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	if err := service.CreateProduct(product); err != nil {
+		Response(http.StatusInternalServerError, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	Response(http.StatusOK, "add product success", nil, c)
+}
 
-func UpdateProduct(c *gin.Context) {}
+func GetProducts(c *gin.Context) {
+	query := &models.Pagination{}
+	if err := c.ShouldBindQuery(query); err != nil {
+		Response(http.StatusBadRequest, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	products, total, err := service.GetProducts(query)
+	if err != nil {
+		Response(http.StatusInternalServerError, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	data := gin.H{
+		"page":  query.Page,
+		"size":  query.Size,
+		"total": total,
+		"data":  products,
+	}
+	Response(http.StatusOK, "get products success", data, c)
+}
 
-func DeleteProductById(c *gin.Context) {}
+func UpdateProduct(c *gin.Context) {
+	var product models.Product
+	if err := c.Bind(&product); err != nil {
+		Response(http.StatusBadRequest, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	if err := service.UpdateProduct(product); err != nil {
+		Response(http.StatusInternalServerError, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	Response(http.StatusOK, "update product success", product, c)
+}
+
+func DeleteProductById(c *gin.Context) {
+	id := c.Param("id")
+	product_id, err := strconv.Atoi(id)
+	if err != nil {
+		Response(http.StatusBadRequest, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	if err = service.DeleteProductById(product_id); err != nil {
+		Response(http.StatusInternalServerError, fmt.Sprintf("%s", err), nil, c)
+		return
+	}
+	Response(http.StatusOK, "delete product success", nil, c)
+}
